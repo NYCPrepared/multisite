a991af4f6366a502c6c00fa4b43fc687798059e5
Pea <pea@misfist.com>
2014-01-24 15:31:55 -0500

    Updated entypo mixin.

--- wp-content/themes/community/plugins/events-manager/templates/events-list.php
+++ wp-content/themes/community/plugins/events-manager/templates/events-list.php
@@ -10,44 +10,8 @@
  */
 $args = apply_filters('em_content_events_args', $args);
 
-if( get_option('dbem_css_evlist') ) 
-	echo "<div class='css-events-list events'>";
-	print<<<_HTML_
-	<table class="events events-table">
-		<thead>
-			<tr>
-				<th>Date/Time</th>
-				<th>Event Title</th>
-				<th>Event Description</th>
-			</tr>
-		</thead>
-		<tbody>
-_HTML_;
+if( get_option('dbem_css_evlist') ) echo "<div class='css-events-list'>";
 
+echo EM_Events::output( $args );
 
-$events = EM_Events::output(array( 
-	'format'=>
-			'<tr>
-				<td>
-				    <h6 class="event-start">
-				        <time class="event-day" datetime="#l">#l</time>
-				        <time class="event-date" datetime="#F #j">#F #j</time>
-				        <time class="event-time" datetime="#g:#i#a">#g:#i#a</time>
-					</h6>
-				</td>
-				<td>
-					<h3 class="event-title post-title">#_EVENTLINK</h3>
-					{has_location}
-						<p class="location-name">#_LOCATIONNAME</p>
-						<p class="location-address">#_LOCATIONADDRESS</p>
-						<p class="location-city-state">#_LOCATIONTOWN #_LOCATIONSTATE</p>
-					{/has_location}
-				</td>
-				<td>#_EVENTEXCERPT{20,...}</td>
-			</tr>'
-	));
-echo $events;
-
-// echo EM_Events::output( $args );
-
-if( get_option('dbem_css_evlist') ) echo "</tbody></table></div>";
+if( get_option('dbem_css_evlist') ) echo "</div>";
