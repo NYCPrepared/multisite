1af1934367a4945ade68dfd6cfc752bae2105029
Erica Heinz <e@energy7.com>
2014-01-24 14:41:37 -0500

    change search submit to button so it can be styled

--- wp-content/themes/community/functions.php
+++ wp-content/themes/community/functions.php
@@ -267,7 +267,7 @@ function bones_wpsearch($form) {
 	$form = '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
 	<label class="screen-reader-text" for="s">' . __( 'Search for:', 'bonestheme' ) . '</label>
 	<input type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="' . esc_attr__( 'Search', 'bonestheme' ) . '" />
-	<input type="submit" id="searchsubmit" value="' . esc_attr__( 'Search' ) .'" />
+	<button type="submit" id="searchsubmit">Search</button>
 	</form>';
 	return $form;
 } // don't remove this bracket!
