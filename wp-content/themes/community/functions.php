77a79ff5f97d00c5b615c6b2cc2c9b3eae51d58f
Pea <pea@misfist.com>
2014-01-26 14:36:28 -0500

    Added custom field for use with volunteer posts

--- wp-content/themes/community/functions.php
+++ wp-content/themes/community/functions.php
@@ -369,5 +369,107 @@ function bones_wpsearch($form) {
 include_once dirname(__FILE__) . '/rewrites.php';
 
 
+/************* CUSTOM METABOX *****************/
+
+/* Location Field for Volunteer Posts */
+
+/* Fire our meta box setup function on the post editor screen. */
+add_action( 'load-post.php', 'community_post_meta_boxes_setup' );
+add_action( 'load-post-new.php', 'community_post_meta_boxes_setup' );
+
+/* Meta box setup function. */
+function community_post_meta_boxes_setup() {
+
+	/* Add meta boxes on the 'add_meta_boxes' hook. */
+	add_action( 'add_meta_boxes', 'community_add_post_meta_boxes' );
+
+	/* Save post meta on the 'save_post' hook. */
+	add_action( 'save_post', 'community_save_volunteer_location_meta', 10, 2 );
+}
+
+/* Create one or more meta boxes to be displayed on the post editor screen. */
+function community_add_post_meta_boxes() {
+
+	add_meta_box(
+		'community-volunteer-location',			// Unique ID
+		esc_html__( 'Volunteer Location', 'example' ),		// Title
+		'community_volunteer_location_meta_box',		// Callback function
+		'post',					// Admin page (or post type)
+		'side',					// Context
+		'default'					// Priority
+	);
+}
+
+/* Display the post meta box. */
+function community_volunteer_location_meta_box( $object, $box ) { ?>
+
+	<?php wp_nonce_field( basename( __FILE__ ), 'community_volunteer_location_nonce' ); ?>
+
+	<p>
+		<label for="community-volunteer-location"><?php _e( "Add a location to your volunteer posts.", 'example' ); ?></label>
+		<br />
+		<input class="widefat" type="text" name="community-volunteer-location" id="community-volunteer-location" value="<?php echo esc_attr( get_post_meta( $object->ID, 'community_volunteer_location', true ) ); ?>" size="30" />
+	</p>
+<?php }
+
+/* Save the meta box's post metadata. */
+function community_save_volunteer_location_meta( $post_id, $post ) {
+
+	/* Verify the nonce before proceeding. */
+	if ( !isset( $_POST['community_volunteer_location_nonce'] ) || !wp_verify_nonce( $_POST['community_volunteer_location_nonce'], basename( __FILE__ ) ) )
+		return $post_id;
+
+	/* Get the post type object. */
+	$post_type = get_post_type_object( $post->post_type );
+
+	/* Check if the current user has permission to edit the post. */
+	if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
+		return $post_id;
+
+	/* Get the posted data and sanitize it for use as an HTML class. */
+	// $new_meta_value = ( isset( $_POST['community-volunteer-location'] ) ? sanitize_html_class( $_POST['community-volunteer-location'] ) : '' );
+	$new_meta_value = $_POST['community-volunteer-location'];
+	
+	/* Get the meta key. */
+	$meta_key = 'community_volunteer_location';
+
+	/* Get the meta value of the custom field key. */
+	$meta_value = get_post_meta( $post_id, $meta_key, true );
+
+	/* If a new meta value was added and there was no previous value, add it. */
+	if ( $new_meta_value && '' == $meta_value )
+		add_post_meta( $post_id, $meta_key, $new_meta_value, true );
+
+	/* If the new meta value does not match the old value, update it. */
+	elseif ( $new_meta_value && $new_meta_value != $meta_value )
+		update_post_meta( $post_id, $meta_key, $new_meta_value );
+
+	/* If there is no new meta value but an old value exists, delete it. */
+	elseif ( '' == $new_meta_value && $meta_value )
+		delete_post_meta( $post_id, $meta_key, $meta_value );
+}
+
+/* Filter the post class hook with our custom post class function. */
+add_filter( 'volunteer_location', 'community_volunteer_location' );
+
+function community_volunteer_location( $locations ) {
+
+	/* Get the current post ID. */
+	$post_id = get_the_ID();
+
+	/* If we have a post ID, proceed. */
+	if ( !empty( $post_id ) ) {
+
+		/* Get the custom post class. */
+		$volunteer_location = get_post_meta( $post_id, 'community_volunteer_location', true );
+
+		/* If a post class was input, sanitize it and add it to the post class array. */
+		if ( !empty( $volunteer_location ) )
+			$locations[] = sanitize_html_class( $volunteer_location );
+	}
+
+	return $locations;
+}
+
 
 ?>
