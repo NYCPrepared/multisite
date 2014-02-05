058766e7134bba830a3529463250fb5e78f4547c
Erica Heinz <e@energy7.com>
2014-02-05 18:08:08 -0500

    add header IDs for JS

--- wp-content/themes/community/header.php
+++ wp-content/themes/community/header.php
@@ -54,10 +54,10 @@
                     	<li><a href="#nav" class="menu-anchor" id="menu-anchor" title="menu"></a></li>
                     	<li><a href="#search" class="search-anchor" id="search-anchor" title="search"></a></li>
                     </ul>
-					<div class="search-form reveal">
+					<div class="search-form reveal" id="search">
 					    <?php get_search_form(); ?>
 					</div>
-					<nav class="main-nav reveal" id="main-navigation" role="navigation">
+					<nav class="main-nav reveal" id="nav" role="navigation">
 						<?php bones_main_nav(); ?>
 					</nav>
 
