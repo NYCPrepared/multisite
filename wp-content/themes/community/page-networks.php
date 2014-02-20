6e955289ca665f1d1ec18788d7bf8c3b1c52a889
Erica Heinz <e@energy7.com>
2014-02-20 16:46:10 -0500

    add  class="page-title" to h1s

--- wp-content/themes/community/page-networks.php
+++ wp-content/themes/community/page-networks.php
@@ -12,7 +12,7 @@
 
 								<header class="article-header">
 
-									<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+									<h1 class="page-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
 
 
 								</header>
