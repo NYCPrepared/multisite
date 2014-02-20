35c75f23a167eff5aecf0e96f6c74505f21cad44
Erica Heinz <e@energy7.com>
2014-02-20 18:04:20 -0500

    style event page

--- wp-content/themes/community/single-event.php
+++ wp-content/themes/community/single-event.php
@@ -12,6 +12,8 @@
 
 								<header class="article-header">
 
+    								<section class="event-image"><?php echo $EM_Event->output('#_EVENTIMAGE'); ?></section>
+    								
 									<h1 class="entry-title single-title event-title" itemprop="headline"><?php echo $EM_Event->output('#_EVENTNAME'); ?></h1>
 									<p class="byline vcard"><?php
 										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&amp;</span> filed under %4$s.', 'bonestheme' ), get_the_time( 'Y-m-j' ), get_the_time( get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', ') );
@@ -19,30 +21,29 @@
 
 								</header>
 
-								<section class="event-image"><?php echo $EM_Event->output('#_EVENTIMAGE'); ?></section>
 
 								<section class="event-details">
-									<div class="date-time">
+									<h4 class="date-time">
 										<span class="event-day"><?php echo $EM_Event->output('#l'); ?></span>
 										<span class="event-date"><?php echo $EM_Event->output('#F #j, #Y'); ?></span>
 										<span class="event-time"><?php echo $EM_Event->output('#g:#i#a'); ?> - <?php echo $EM_Event->output('#@g:#@i#@a'); ?></span>
-									</div>
+									</h4>
 
 									<?php if($EM_Event->location_id) { ?>
-									<div class="location">
-										<div class="location-name"><?php echo $EM_Event->output('#_LOCATIONLINK'); ?></div>
-										<div class="location-address"><?php echo $EM_Event->output('#_LOCATIONTOWN #_LOCATIONSTATE'); ?></div>
-									</div>
+									<h4 class="location">
+										<span class="location-name"><?php echo $EM_Event->output('#_LOCATIONLINK'); ?></span>
+										<span class="location-address"><?php echo $EM_Event->output('#_LOCATIONTOWN #_LOCATIONSTATE'); ?></span>
+									</h4>
 									<?php } ?>
 
 									<div class="tools"><a href="<?php echo $EM_Event->output('#_EVENTICALURL'); ?>" class="add-to-calendar button">Add to Calendar</a></div>
 								</section>
 
+								<section class="event-description"><?php echo $EM_Event->output('#_EVENTNOTES'); ?></section>
+
 								<?php if($EM_Event->location_id) { ?>
 								<section class="event-map"><?php echo $EM_Event->output('#_MAP'); ?></section>
 								<?php } ?>
-
-								<section class="event-description"><?php echo $EM_Event->output('#_EVENTNOTES'); ?></section>
 
 								<footer class="event-footer">
 									<div class="meta categories"><?php echo $EM_Event->output('#_EVENTCATEGORIES'); ?></div>
