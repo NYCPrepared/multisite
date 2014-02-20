c6b53ab848690811c63ee5109d3b16cc08e457f9
Erica Heinz <e@energy7.com>
2014-02-20 17:10:18 -0500

    responsive styling for events search

--- wp-content/themes/community/index.php
+++ wp-content/themes/community/index.php
@@ -12,7 +12,7 @@
 
 								<header class="article-header">
 
-									<h1 class="page-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+									<h1 class="page-title"><?php the_title(); ?></h1>
 									<!-- <p class="byline vcard"><?php
 										printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
 									?></p> -->
