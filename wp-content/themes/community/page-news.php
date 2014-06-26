d81c0e2f732147adadb44ec8b1aa28e23ed7c2e4
Erica Heinz <e@energy7.com>
2014-06-25 20:07:17 -0400

    change networks to use square images

--- wp-content/themes/community/page-news.php
+++ wp-content/themes/community/page-news.php
@@ -1,63 +1,65 @@
 <?php get_header(); ?>
 
-			<div id="content">
+	<div id="content">
 
-				<div id="inner-content" class="wrap clearfix">
+		<div id="inner-content" class="wrap clearfix">
 
-						<div id="intro" class="first clearfix" role="intro">
+			<div id="intro" class="first clearfix" role="intro">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-								<header class="article-header">
+					<header class="article-header">
 
-									<h1 class="post-title" itemprop="headline"><?php the_title(); ?></h1>
+						<h1 class="post-title" itemprop="headline"><?php the_title(); ?></h1>
 
-								</header>
+					</header>
 
-								<section class="entry-content clearfix" itemprop="articleBody">
-									<?php the_content(); ?>
-								</section>
+					<section class="entry-content clearfix" itemprop="articleBody">
+						<?php the_content(); ?>
+					</section>
 
-								<footer class="article-footer">
-									<?php the_tags( '<h6 class="tags">' . __( 'Tags:', 'bonestheme' ) . '</h6> ', ', ', '' ); ?>
+					<footer class="article-footer">
+						<?php the_tags( '<h6 class="tags">' . __( 'Tags:', 'bonestheme' ) . '</h6> ', ', ', '' ); ?>
 
-								</footer>
+					</footer>
 
-							</article>
+				</article>
 
-							<?php endwhile; endif; ?>
-
-						</div>
-						<div id="main" class="content news-feed first clearfix" role="main">
-						<?php
-						if(function_exists( 'network_latest_posts' )) {
-
-							$parameters = array(
-							'title_only'    => 'false',
-							'auto_excerpt'  => 'true',
-							'full_meta'		=> 'true',
-							'show_categories'    => 'true', 
-							'display_type'		=> 'block',
-							'thumbnail'        => 'true',
-							'thumbnail_wh'	   => 'medium',
-							'thumbnail_class'  => 'post-image',
-							'wrapper_list_css' => 'post-list',
-							'wrapper_block_css'=> 'news', //The wrapper classe
-							'instance'         => 'news-page', //The wrapper ID
-							'paginate'         => 'true',        // Paginate results
-					        'posts_per_page'   => 25, 
-							);
-							// Execute
-							$posts = network_latest_posts($parameters);
-						}
-						?>
-
-						</div>
-
-				</div>
+				<?php endwhile; endif; ?>
 
 			</div>
+			
+			
+			<div id="main" class="content news-feed first clearfix" role="main">
+			<?php
+			if(function_exists( 'network_latest_posts' )) {
+
+				$parameters = array(
+				'title_only'    => 'false',
+				'auto_excerpt'  => 'true',
+				'full_meta'		=> 'true',
+				'show_categories'    => 'true', 
+				'display_type'		=> 'block',
+				'thumbnail'        => 'true',
+				'thumbnail_wh'	   => 'medium',
+				'thumbnail_class'  => 'post-image',
+				'wrapper_list_css' => 'post-list',
+				'wrapper_block_css'=> 'news', //The wrapper classe
+				'instance'         => 'news-page', //The wrapper ID
+				'paginate'         => 'true',        // Paginate results
+		        'posts_per_page'   => 25, 
+				);
+				// Execute
+				$posts = network_latest_posts($parameters);
+			}
+			?>
+
+			</div>
+
+		</div>
+
+	</div>
 
 <?php get_footer(); ?>
