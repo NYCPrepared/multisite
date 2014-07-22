3a15117f0867e5b11f68e00e9309778431be7fa8
Pea <pea@misfist.com>
2014-07-22 19:59:32 -0400

    Removed menu locations on child themes.

--- wp-content/themes/community-group/functions.php
+++ wp-content/themes/community-group/functions.php
@@ -16,8 +16,21 @@ add_theme_support( 'custom-header', $args );
 
 // Remove theme customization settings for child theme
 
-remove_action( 'customize_register', 'community_customize_register' );
+remove_action( 'customize_register', 'community_customize_register', 20 );
 
+
+// Remove menu customization on child themes
+
+add_action( 'after_setup_theme', 'remove_theme_customization_community_group', 20 ); 
+
+function remove_theme_customization_community_group() {
+
+    unregister_nav_menu( 'main-nav' );
+	unregister_nav_menu( 'secondary-nav' );
+	unregister_nav_menu( 'utility-nav' );
+	unregister_nav_menu( 'footer-links' );
+
+}
 
 
 ?>
