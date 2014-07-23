5ba00ca09563b0637513b79f37eec25f0fa7d075
Pea <pea@misfist.com>
2014-07-22 20:40:35 -0400

    Registered a new menu for the Group site, to distinguish from the global WP network menu.

--- wp-content/themes/community-group/functions.php
+++ wp-content/themes/community-group/functions.php
@@ -29,6 +29,11 @@ function remove_theme_customization_community_group() {
 	unregister_nav_menu( 'secondary-nav' );
 	unregister_nav_menu( 'utility-nav' );
 	unregister_nav_menu( 'footer-links' );
+	register_nav_menus(
+		array(
+			'site-nav' => __( 'The Main Site Menu', 'community-group' ),
+		)
+	);
 
 }
 
