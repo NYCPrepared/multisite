be3ca7054cf015b51390ee7c079b28854d77774d
Erica Heinz <e@energy7.com>
2014-08-18 00:23:14 -0400

    restyle partner page

--- wp-content/themes/community-partner/header.php
+++ wp-content/themes/community-partner/header.php
@@ -80,14 +80,15 @@
 
 			<div class="wrap">
 
-				<div class="site-banner">
-				    <div class="banner-inner">
-					    <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
-				    </div>
-                    <h1 class="site-name"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
+		    <div class="partner-logo">
+			    <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+		    </div>
+		    
+				<div class="partner-banner">
+            <h5 class="site-prefix">News from</h5>
+            <h1 class="site-name"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
+            <?php // bloginfo('description'); ?>
 				</div>
-				<?php // bloginfo('description'); ?>
-
 
 			</div>
 
