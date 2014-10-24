4b20b8674fc9f71b6a93594334092bb99df213b0
Pea <pea@misfist.com>
2014-10-24 14:28:35 -0400

    Removed all files except wp-content directory.

--- wp-content/plugins/events-manager/templates/forms/ticket-form.php
+++
@@ -1,3 +0,0 @@
-<?php 
-//DEPRECATED, MODIFY THE INCLUDED TEMPLATE INSTEAD
-include('event/bookings-ticket-form.php');  	
--- wp-content/themes/community/partials/error-multisite.php
+++
@@ -1,4 +0,0 @@
-<article id="home-error" class="module row error">
-	<h2 class="module-heading">Sorry</h2>
-	<p>This theme requires <a href="http://codex.wordpress.org/Create_A_Network" target="_blank">WP multisite.</p>
-</article>
